8abbde76538a7536a645cbec3d00d6a4350acdd7
Brittany Morris <brittanymorrisdesign@gmail.com>
2019-12-29 15:57:35 -0500

    Set up variables

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
  
+  var apiKey = "06606e544a946ac567964601f7ed0813";
+  var cityName = "";
+  var cityLat = "";
+  var cityLong = "";
+  var cityArray =[];
 
-  
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+  api.openweathermap.org/data/2.5/weather?q=
+
+
   });
